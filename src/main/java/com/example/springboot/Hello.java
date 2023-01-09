8e4b4eea0cb09a93d13fa8134119133a8bf9c8fa
zainabnoori <zainab.noori@and.digital>
2023-01-09 12:49:00 +0000

    Initialise spring boot project

---
+++ src/main/java/com/example/springboot/Hello.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello from Spring Boot!";
+    }
+
+}
